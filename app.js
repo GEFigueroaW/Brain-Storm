520986f4c3539da522db514f1c282ee4bf05005d
GEFigueroaW <eugenfw@gmail.com>
2025-06-22 12:25:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // =================== CONFIGURACIÓN FIREBASE ===================
 const firebaseConfig = {
-  apiKey: "AQUI_TU_API_KEY",
-  authDomain: "AQUI_TU_AUTH_DOMAIN",
-  projectId: "AQUI_TU_PROJECT_ID",
-  appId: "AQUI_TU_APP_ID"
+  apiKey: "AIzaSyCScJA-UGs3WcBnfAm-6K94ybZ4bzBahz8",
+  authDomain: "brain-storm-8f0d8.firebaseapp.com",
+  databaseURL: "https://brain-storm-8f0d8-default-rtdb.firebaseio.com",
+  projectId: "brain-storm-8f0d8",
+  storageBucket: "brain-storm-8f0d8.appspot.com",
+  messagingSenderId: "401208607043",
+  appId: "1:401208607043:web:6f35fc81fdce7b3fbeaff6"
 };
 
 firebase.initializeApp(firebaseConfig);
